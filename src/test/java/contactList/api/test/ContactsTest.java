3a5f9a8cfec706fbb18b56bb7421375f0407f0b7
ctrlshiftsr <hsowmya305@gmail.com>
2025-03-26 18:17:54 +0530

    removed commented lines

--- src/test/java/contactList/api/test/ContactsTest.java
+++ src/test/java/contactList/api/test/ContactsTest.java
@@ -15,15 +15,16 @@ public class ContactsTest {
     Contacts contactsPayload;
     String userToken;
     String contactId;
+
     @BeforeClass
-    public void setup(){
+    public void setup() {
         UserTest userTest = new UserTest();
 
         faker = new Faker();
-        contactsPayload= new Contacts();
+        contactsPayload = new Contacts();
         userTest.setUpData();
         userTest.testPostUser();
-        userToken= userTest.getToken();
+        userToken = userTest.getToken();
 
         contactsPayload.setFirstName(faker.name().firstName());
         contactsPayload.setLastName(faker.name().lastName());
@@ -32,32 +33,36 @@ public class ContactsTest {
         contactsPayload.setEmail(faker.internet().emailAddress());
 
     }
-    @Test(priority = 1,groups = "smoke")
-    public void testAddContacts(){
 
-            Response response = ContactsEndPoints.addContacts(contactsPayload,userToken);
-            contactId = response.jsonPath().getString("_id");
-            response.then().log().all();
+    @Test(priority = 1, groups = "smoke")
+    public void testAddContacts() {
 
-            Assert.assertEquals(response.getStatusCode(),201,"User not created");
+        Response response = ContactsEndPoints.addContacts(contactsPayload, userToken);
+        contactId = response.jsonPath().getString("_id");
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(), 201, "User not created");
 
     }
-    @Test(priority = 2,groups = "sanity")
-    public void testReadContactList(){
+
+    @Test(priority = 2, groups = "sanity")
+    public void testReadContactList() {
         Response response = ContactsEndPoints.readContactList(userToken);
         response.then().log().all();
 
-        Assert.assertEquals(response.getStatusCode(),200,"Cannot retrieve contact List");
+        Assert.assertEquals(response.getStatusCode(), 200, "Cannot retrieve contact List");
     }
-    @Test(priority = 3,groups = "sanity")
-    public void testReadContacts(){
+
+    @Test(priority = 3, groups = "sanity")
+    public void testReadContacts() {
         Response response = ContactsEndPoints.readContact(userToken);
         response.then().log().all();
 
-        Assert.assertEquals(response.getStatusCode(),200,"Cannot get contact details");
+        Assert.assertEquals(response.getStatusCode(), 200, "Cannot get contact details");
     }
-    @Test(priority = 4,groups = "regression")
-    public void testUpdateContact(){
+
+    @Test(priority = 4, groups = "regression")
+    public void testUpdateContact() {
 
         contactsPayload.setFirstName(faker.name().firstName());
         contactsPayload.setLastName(faker.name().lastName());
@@ -70,38 +75,37 @@ public class ContactsTest {
         contactsPayload.getStreet1();
         contactsPayload.getStreet2();
         contactsPayload.getStateProvince();
-        Response response = ContactsEndPoints.updateContact(contactsPayload,userToken,contactId);
+        Response response = ContactsEndPoints.updateContact(contactsPayload, userToken, contactId);
         response.then().log().all();
 
-        Assert.assertEquals(response.getStatusCode(),200," user details Cannot be update");
+        Assert.assertEquals(response.getStatusCode(), 200, " user details Cannot be update");
 
         Response responseAfterUpdate = ContactsEndPoints.readContact(userToken);
 
-        Assert.assertEquals(responseAfterUpdate.getStatusCode(),200,"User details are not updated");
+        Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200, "User details are not updated");
     }
 
-    @Test(priority = 5,groups = "regression")
-    public void testUpdateContactFirstName(){
+    @Test(priority = 5, groups = "regression")
+    public void testUpdateContactFirstName() {
         contactsPayload.setFirstName(faker.name().firstName());
-        Response response = ContactsEndPoints.updateContactFirstName(contactsPayload,userToken,contactId);
+        Response response = ContactsEndPoints.updateContactFirstName(contactsPayload, userToken, contactId);
         response.then().log().all();
 
-        Assert.assertEquals(response.getStatusCode(),200,"FirstName cannot be updated");
+        Assert.assertEquals(response.getStatusCode(), 200, "FirstName cannot be updated");
 
         Response responseAfterUpdateFirstName = ContactsEndPoints.readContact(userToken);
         responseAfterUpdateFirstName.then().log().all();
 
-        Assert.assertEquals(responseAfterUpdateFirstName.getStatusCode(),200,"First name cannot be updated");
+        Assert.assertEquals(responseAfterUpdateFirstName.getStatusCode(), 200, "First name cannot be updated");
     }
-    @Test(priority = 6,groups = "smoke")
-    public void testdeleteContacts(){
-        Response response = ContactsEndPoints.deleteContacts(userToken,contactId);
+
+    @Test(priority = 6, groups = "smoke")
+    public void testdeleteContacts() {
+        Response response = ContactsEndPoints.deleteContacts(userToken, contactId);
         response.then().log().all();
 
-        Assert.assertEquals(response.getStatusCode(),200,"Contact details cannot be deleted");
+        Assert.assertEquals(response.getStatusCode(), 200, "Contact details cannot be deleted");
     }
-
-
 
 
 }
--- src/test/java/contactList/api/test/UserTest.java
+++ src/test/java/contactList/api/test/UserTest.java
@@ -44,55 +44,55 @@ public class UserTest {
         Assert.assertEquals(response.getStatusCode(), 201, "User not created");
     }
 
-//    @Test(priority = 2, groups = "sanity")
-//    public void testGetUser() {
-//        Response response = UserEndPoints.readUser(token);
-//        response.then().log().all();
-//        String id1 = response.jsonPath().getString("_id");
-//        Assert.assertEquals(response.getStatusCode(), 200, "Cannot access user details");
-//        Assert.assertEquals(id1, id, "user did not match");
-//    }
-//
-//    @Test(priority = 3, groups = "regression")
-//    public void testUpdateUser() {
-//        userPayload.setFirstName(faker.name().firstName());
-//        userPayload.setLastName(faker.name().lastName());
-//        Response response = UserEndPoints.updateUser(userPayload, token);
-//        response.then().log().all();
-//        Assert.assertEquals(response.getStatusCode(), 200, "User details not updated");
-//
-//        Response responseAfterUpdate = UserEndPoints.readUser(token);
-//
-//        Assert.assertEquals(responseAfterUpdate.statusCode(), 200, "User details not updated ");
-//    }
-//
-//    @Test(priority = 4, groups = "sanity")
-//    public void testLoginUser() {
-//        Response response = UserEndPoints.loginUser(userPayload, token);
-//        loginToken = response.jsonPath().getString("token");
-//        loginUserId = response.jsonPath().getString("user._id");
-//        System.out.println(loginToken);
-//        response.then().log().all();
-//        Assert.assertEquals(response.getStatusCode(), 200, "Login not successful");
-//
-//    }
-//
-//    @Test(priority = 5, groups = "sanity")
-//    public void testLogoutUser() {
-//        Response response = UserEndPoints.logoutUser(token);
-//        response.then().log().all();
-//        Assert.assertEquals(response.getStatusCode(), 200, "User logout unsuccessful");
-//    }
-//
-//    @Test(priority = 6, groups = "smoke")
-//    public void testDeleteUser() {
-//        this.testLoginUser();
-//        Response response = UserEndPoints.deleteUser(loginToken);
-//        System.out.println("this is for delete");
-//        System.out.println(loginToken);
-//        response.then().log().all();
-//        Assert.assertEquals(response.getStatusCode(), 200, "Delete user unsuccessful");
-//    }
+    @Test(priority = 2, groups = "sanity")
+    public void testGetUser() {
+        Response response = UserEndPoints.readUser(token);
+        response.then().log().all();
+        String id1 = response.jsonPath().getString("_id");
+        Assert.assertEquals(response.getStatusCode(), 200, "Cannot access user details");
+        Assert.assertEquals(id1, id, "user did not match");
+    }
+
+    @Test(priority = 3, groups = "regression")
+    public void testUpdateUser() {
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        Response response = UserEndPoints.updateUser(userPayload, token);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200, "User details not updated");
+
+        Response responseAfterUpdate = UserEndPoints.readUser(token);
+
+        Assert.assertEquals(responseAfterUpdate.statusCode(), 200, "User details not updated ");
+    }
+
+    @Test(priority = 4, groups = "sanity")
+    public void testLoginUser() {
+        Response response = UserEndPoints.loginUser(userPayload, token);
+        loginToken = response.jsonPath().getString("token");
+        loginUserId = response.jsonPath().getString("user._id");
+        System.out.println(loginToken);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200, "Login not successful");
+
+    }
+
+    @Test(priority = 5, groups = "sanity")
+    public void testLogoutUser() {
+        Response response = UserEndPoints.logoutUser(token);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200, "User logout unsuccessful");
+    }
+
+    @Test(priority = 6, groups = "smoke")
+    public void testDeleteUser() {
+        this.testLoginUser();
+        Response response = UserEndPoints.deleteUser(loginToken);
+        System.out.println("this is for delete");
+        System.out.println(loginToken);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200, "Delete user unsuccessful");
+    }
 
 
 }
